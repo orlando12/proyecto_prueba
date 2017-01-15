62435dd3079a63619ac496800a36946ae59bf14c
Tzec <Tzec@172.16.16.105>
2017-01-15 10:31:17 -0600

    cambio

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -5,6 +5,8 @@
  */
 package prueba;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Tzec
@@ -15,7 +17,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Prueba");
     }
     
 }
